fef3323a9504db06b1d913a2130c8e7e075b2efe
Sufyan Kamil <sufyankamil15@gmail.com>
2023-09-24 23:40:04 +0530

    Added search func

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,124 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _searchController = TextEditingController();
+
+  bool isShowUsers = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.dispose();
+  }
+
+  String? _validateSearchText(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a search query';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: TextFormField(
+            controller: _searchController,
+            decoration: const InputDecoration(
+              labelText: 'Search a user ',
+              hintText: 'Search a user',
+              border: OutlineInputBorder(),
+            ),
+            validator: _validateSearchText,
+            onFieldSubmitted: (String _) {
+              setState(() {
+                isShowUsers = true;
+              });
+            },
+          ),
+        ),
+        body: isShowUsers
+            ? FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection('users')
+                    .where(
+                      'username',
+                      isGreaterThanOrEqualTo: _searchController.text,
+                    )
+                    .get(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    );
+                  }
+                  return ListView.builder(
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {},
+                        // onTap: () => Navigator.of(context).push(
+                        //   MaterialPageRoute(
+                        //     builder: (context) => ProfileScreen(
+                        //       uid: (snapshot.data! as dynamic).docs[index]
+                        //           ['uid'],
+                        //     ),
+                        //   ),
+                        // ),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              (snapshot.data! as dynamic).docs[index]
+                                  ['photoUrl'],
+                            ),
+                            radius: 16,
+                          ),
+                          title: Text(
+                            (snapshot.data! as dynamic).docs[index]['username'],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              )
+            : FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection('posts')
+                    .orderBy('datePublished')
+                    .get(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    );
+                  }
+
+                  return MasonryGridView.count(
+                    crossAxisCount: 3,
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                    itemBuilder: (context, index) => Image.network(
+                      (snapshot.data! as dynamic).docs[index]['postUrl'],
+                      fit: BoxFit.cover,
+                    ),
+                    mainAxisSpacing: 8.0,
+                    crossAxisSpacing: 8.0,
+                  );
+                },
+              ),
+      ),
+    );
+  }
+}
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
 import 'package:instagram_clone/screens/feed_screen.dart';
 
+import '../screens/search_screen.dart';
+
 const webScreen = 600;
 
 List<Widget> homeScreenItems = [
   const FeedScreen(),
-  Text('Search'),
+  const SearchScreen(),
   const AddPost(),
 ];
